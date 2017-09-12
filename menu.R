7eadcce724571f5ee0ccf26cc618ad1d986db66a
jarasmen <jareczek14@gmail.com>
2017-09-12 21:36:12 +0200

    Add files via upload
    Funkcje wyboru przeniesione z menu do osobnego pliku

--- menu.R
+++ menu.R
@@ -2,15 +2,38 @@ source('testy/BAT zrobione funkcje.R')
 source('testy/DE funkcje.R')
 source('testy/GA funkcje.R')
 source('testy/PSO funkcje.R')
+source('testy/wybory.R')
 
-print("Wybierz algorytm do optymalizacji wprowadzajac odpowiednia cyfre")
+
+print("Wybierz algorytm do optymalizacji")
+print("1.BAT - alg.nietoperza")
+print("2.DE - alg.ewolucji roznicowej")
+print("3.GA - alg.genetyczny")
+print("4.PSO - alg.roju czastek")
+
+#Wywolanie funkcji wyboru algorytmu
+algorytmy()
+
 cat("\n")
-print("1.BAT-alg.nietoperza")
-print("2.DE-alg.ewolucji roznicowej")
-print("3.GA-alg.genetyczny")
-print("4.PSO-alg.roju czastek")
+print("Lista dostepnych funkcji")
+print("1. Ackley")
+print("2. Beale")
+print("3. Goldstein")
+print("4. Bartels-Conn")
+print("5. Leon")
+print("6. Eggholder")
+print("7. Venter")
+print("8. Matyas")
+print("9. Zirilli")
+print("10. Easom")
+print("11. Rastrigin")
+print("12. Levin13")
+print("13. Drop Wave")
 
-alg = readline(prompt="Wybor algorytmu: ")
+
+#Wywolanie funkcji wyboru ilosci powtorzen i funkcji
+wybor_funkcji()
+powtorzenia()
 
 if (alg==1){
   source('testy/test BAT.R')
