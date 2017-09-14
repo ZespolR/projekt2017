5403820a2988f0258770b05c4a7798d9c8281f9e
Jonasz <jonkul001@gmail.com>
2017-09-14 17:13:04 +0200

    final 2

--- testy/programDEopisany.R
+++ testy/programDEopisany.R
@@ -1,61 +1,53 @@
-# for reproducive results
-#fcja=function(funkcja){
-#fit <- bat_optim(D = 2, NP = il.robali[j], N_Gen = 100, A = 0.5, r = 0.5,
-#      Qmin = 0, Qmax = 2, Lower = xmin, Upper = xmax, FUN = funkcja, anim=FALSE,set.seed(runif(1,min=0,max=1000)))}
-
-library(DEoptim)
-library(data.table)
-library(xlsx)
 
 
-funkcje=list(ackley.de,#[-32,32] min 0 at(0,0) 
-             #lista- rodzaj pojemnika na dane, potrzebne żeby jak sie wywołuje algorytm de  to żeby nie trzeba było wpisywać nazwy funkcji tylko żeby sie to robiło automatycznie, patrz tam niżej po for(i in 1:powt) w wywoluaniu DE, tam jest funkcje[[k]], czyli kiedy np. k=2 to będzie poszukiwanie min funkjci cross in try
-             cross_in_tray.de, # x[-10,10] min -2.06 at (1.34,1.34 i z minusami)
-             drop_wave.de, # [-5.12,5.12] min -1 at (0,0)
-             beale.de,#[-4.5,4.5] min 0 at (3,0.5)
-             eggholder.de,# [-512,512] min -959 at (512,404)
-             holder.table.de, #x[-10,10] min -19 at (8.05,9.66 i z minusami)
-             matyas.de,# [-10,10] min 0 at (0,0)
-             easom.de,# [-100,100] min -1 at (3.14,3.14)
-             rastrigin.de,# x[-5.12,5.12] min 0 at (0,0)
-             schaffer.de, # [-100,100] min 0 at (0,0)
-             six.hump.camel.de, #x1[-3,3] x2[-2,2] min -1.03 at (0.089,-0.712) lub(-0.089,   0.71)
-             three.hump.camel.de,# [-5,5] min 0 at (0,0)
-             levin13.de# [-10,10] min 0 at (1,1) 
+library(DEoptim)        #uruchomienie pakietu z wybranym algorytmem
+library(data.table)     #uruchomienie pakietu z tabela przechowujaca wyniki w R
+library(xlsx)           #pakiet eksportujacy tabele do MS Excel
+
+# funkcje optymalizowane w programie, w nawiasie podane ogranicznia szukanych niewiadomych i szukanego minimum
+funkcje=list(ackley.pso,#[-32,32] min 0 at(0,0)
+             beale.pso,#[-4.5,4.5] min 0 at (3,0.5)
+             goldstein.pso,# [-2,2] min 3 at (0,-1)
+             bartels.conn.pso,# [-500,500] min 1 at (0,0)
+             leon.pso,# [-1.2,1.2] min 0 at (1,1)
+             eggholder.pso,# [-512,512] min -959 at (512,404)
+             venter.pso,# [-50,50] min -400 at (0,0)
+             matyas.pso,# [-10,10] min 0 at (0,0)
+             zirilli.pso,#[-10,10] min -0.35 at (-1.04,0)
+             easom.pso,# [-100,100] min -1 at (3.14,3.14)
+             rastrigin.pso,# x[-5.12,5.12] min 0 at (0,0)
+             levin13.pso,# [-10,10] min 0 at (1,1)
+             drop_wave.pso# [-5.12,5.12] min -1 at (0,0)
 )
 
-funkcje.nazwa=list("ackley.de",#[-32,32] min 0 at(0,0)
-                   #tutaj jest lista z nazwami, potrzebne do nazywania plików excela , na samym dole. np. jak k=13 to funkcje.nazwa[[k]] będzie oznaczało nazwe"levin13.de"
-                   "cross_in_tray.de", # x[-10,10] min -2.06 at (1.34,1.34 i z minusami)
-                   "drop_wave.de", # [-5.12,5.12] min -1 at (0,0)
-                   "beale.de",#[-4.5,4.5] min 0 at (3,0.5)
-                   "eggholder.de",# [-512,512] min -959 at (512,404)
-                   "holder.table.de", #x[-10,10] min -19 at (8.05,9.66 i z minusami)
-                   "matyas.de",# [-10,10] min 0 at (0,0)
-                   "easom.de",# [-100,100] min -1 at (3.14,3.14)
-                   "rastrigin.de",# x[-5.12,5.12] min 0 at (0,0)
-                   "schaffer.de", # [-100,100] min 0 at (0,0)
-                   "six.hump.camel.de", #x1[-3,3] x2[-2,2] min -1.03 at (0.089,-0.712) lub(-0.089,   0.71)
-                   "three.hump.camel.de",# [-5,5] min 0 at (0,0)
-                   "levin13.de"# [-10,10] min 0 at (1,1)
+funkcje.nazwa=list("ackley.pso",#[-32,32] min 0 at(0,0)
+                   "beale.pso",#[-4.5,4.5] min 0 at (3,0.5)
+                   "goldstein.pso",# [-2,2] min 3 at (0,-1)
+                   "bartels.conn.pso",# [-500,500] min 1 at (0,0)
+                   "leon.pso",# [-1.2,1.2] min 0 at (1,1)
+                   "eggholder.pso",# [-512,512] min -959 at (512,404)
+                   "venter.pso",# [-50,50] min -400 at (0,0)
+                   "matyas.pso",# [-10,10] min 0 at (0,0)
+                   "zirilli.pso",#[-10,10] min -0.35 at (-1.04,0)
+                   "easom.pso",# [-100,100] min -1 at (3.14,3.14)
+                   "rastrigin.pso",# x[-5.12,5.12] min 0 at (0,0)
+                   "levin13.pso",# [-10,10] min 0 at (1,1)
+                   "drop_wave.pso"# [-5.12,5.12] min -1 at (0,0)
 )
 
-zakres=c(32,10,5.12,4.5,512,10,10,100,5.12,100,3,5,10) # wektor czyli pojemnik innego rodzaju zawierający zakres poszukiwania,tak jak np. dla ackley(32,32), teraz jak wpiszemy zakres[3] to wyskoczy 5.12
-min=c(0,2.06,1,0,959,19,0,1,0,0,1.03,0,0)# wektor z minimami ktore mają wyjsc chyba nie sa uzywane teraz w programie do niczego
+zakres=c(32,4.5,2,500,1.2,512,50,10,10,100,5.12,10,5.12)#wektor ograniczen szukanych niewiadomych i szukanego minimum
+min=c(0,0,3,1,0,-959,-400,0,-0.35,-1,0,0,-1) # szukane minima kolejnych funkcji
 
-il.robali <- c(30,60,80,100,150) #wektor z il robali
+il.robali <- c(30,60,80,100,150) # liczebnosci rojow
 
-powt = 50 #ile razy ma powtrzyc 
-
-for(k in 1:13) #najpierw k =1, robi caly kod objety tymi klamrami { } kiedy zrobi, zwieksza k o 1 znowu robi i tak az do 13
+for(k in wybor:wybor)     # optymalizacja funkcji w zaleznosci od wyboru
 {
   
-  
-    xmin <- -zakres[k] # kiedy np. k=3, to -zakres[k] = -5.12 i taka wartosc ma teraz xmin
+    xmin <- -zakres[k] 
     xmax <- zakres[k]
   
   
-  x1=c()    # wektory potrzebne do trzymania wynikow
+  x1=c()      # wektory potrzebne do przechowywania wynikow
   x2=c()
   minimum=c()
   czas=c()
@@ -72,9 +64,9 @@ for(k in 1:13) #najpierw k =1, robi caly kod objety tymi klamrami { } kiedy zrob
   blad.sk.sr=c()
   odchylenie.sr=c()
   
-  for(j in 1:5) #jest 5 liczebnosci rojow i dla kazdego beda powtorzenia
+  for(j in 1:5) # iteracja dla kazdej z liczebnosci rojow
   {
-    x1=0 # zerowanie czyli usuwanie starych danych z wektorow po kazdej petli
+    x1=0 # zerowanie wynikow
     x2=0
     minimum=0
     czas=0
@@ -83,43 +75,66 @@ for(k in 1:13) #najpierw k =1, robi caly kod objety tymi klamrami { } kiedy zrob
     odchylenie=0
     
     
-    for(i in 1:powt) #powt jest rowne 50 wiec bedzie robic 50 razy od i=1 do i=50
-    {
-      
-      if(k==11) # akurat k=11 to six humpcamel, on ma 2 rozne zakresy, wiec trzeba inaczej postapic z lower iupper jesli k =11 to robi to co jest w klamrach{ }
+    for(i in 1:powt) # ilosc iteracji uzalezniona od liczby powtorzen
+      if(i==powt && j==5) # jesli liczebnosc roju wynosi 200 i powtarzana jest ostatnia iteracja to generowane sa wykresy
       {
-        lower = c(-3, -2)
-        upper = -lower
+        lower <- c(xmin, xmin)
+        upper <- -lower
+        set.seed(runif(1,min=0,max=1000))
+        
+        ptm = proc.time() 
+        czas.stracony =0
+        
+        outDEoptim = DEoptim(fn=funkcje[[k]], lower, upper,
+                             DEoptim.control(itermax = 100,storepopfrom = 1, storepopfreq = 5, NP=il.robali[j],trace=FALSE)) #wywolanie algorytmu DE
+        #czas
+        wynikczasu<- proc.time() - ptm
+        
+        for(z in 2:(outDEoptim$optim$iter/5 - 5))
+        {
+          jpeg(paste("C:/Users/Konasz/Dysk Google/ETI/proj/de_",funkcje.nazwa[k],"_po_optym/",z,".jpg",sep="")) #sciezka pod jaka zapisywany bedzie wykres roju
+          
+          plot(outDEoptim$member$storepop[[z]],main="Populacja DE",type = "p",pch=1,col="darkblue",cex=2,xlab="x1", ylab="x2",xlim=range(xmin:xmax),ylim=range(xmin:xmax))                    #rysowanie wykresu
+          dev.off()
+        }
+        jpeg(paste("C:/Users/Konasz/Dysk Google/ETI/proj/de_",funkcje.nazwa[k],"_po_optym/wykres.jpg",sep=""))#sciezka do wykresu zaleznosci znalezionego minimum od iteracji
+        
+        plot(outDEoptim$member$bestvalit, type = 'o', col = 'black',xlab="Iteracje", ylab="Wartosci minimum")    #rysowanie wykresu
+        
+        dev.off()
+        
       }
-      else# jesli k nie rowna sie 11 to robi normalnie, to co jest ponizej w klamrach
-      {
+    
+    else{ # opcja bez tworzenia wykresow
+      
+    {
+     
         lower = c(xmin, xmin)
         upper = -lower
-      }
+      
         set.seed(runif(1,min=0,max=1000)) 
         
-        ptm = proc.time() # do liczenia czasu, czas poczatkowy
+        ptm = proc.time() # czas poczatkowy
         
-        outDEoptim = DEoptim(fn=funkcje[[k]], lower, upper,# to jest wywolanie algorymu DE, tu trzeba zmienic jak bedziesz chcial firefly
+        outDEoptim = DEoptim(fn=funkcje[[k]], lower, upper, # wywolanie algorymu DE
                              DEoptim.control(itermax = 100,storepopfrom = 1, storepopfreq = 5, NP=il.robali[j],trace=FALSE))
-        #czas
-        wynikczasu= proc.time() - ptm#wynik czasu = czas teraz- czas przed wykonaniem DE
         
+        wynikczasu= proc.time() - ptm # czas = czas teraz- czas przed wykonaniem DE
+    }
       
-      
-      x1[i] = outDEoptim$optim$bestmem[1]# znaleziona wspolrzedana x1, to trzeba znalezc w algorytmie samemu summaryDE podsumowuje ci wszystkie wyniki a zeby dobrac sie do tego konkretnego trzeba kombinowac
+      x1[i] = outDEoptim$optim$bestmem[1]# znaleziona wspolrzedana x1
       x2[i] = outDEoptim$optim$bestmem[2]# x2
-      minimum[i] = outDEoptim$optim$bestval  ## UWAGA TU W ZALEZNOSCI OD ALGORYTMU -1 ALBO NIE !!!!!!! to jest minimum
-      czas[i] = wynikczasu[3]
-      iter[i] = outDEoptim$optim$iter #iteracje
-      #blad sredniokwadratowy
-      blad.sk[i] = (minimum[i] + min[k])^2## UWAGA TU W ZALEZNOSCI OD ALGORYTMU + ALBO NIE !!!!!!!
+      minimum[i] = outDEoptim$optim$bestval # znalezione minimum funkcji
+      czas[i] = wynikczasu[3]            # czas wykonania powtorzenia
+      iter[i] = outDEoptim$optim$iter    # ilosc iteracji algorytmu do wyniku
       
-    }#koniec petli z i
-    #odchylenie standardowe
-    odchylenie[j] = sd(minimum)
+      blad.sk[i] = (minimum[i] + min[k])^2#blad sredniokwadratowy
+      
+    }
     
-    x1[powt+1] = mean(x1) #obliczanie srednich z 50 prob mean to srednia
+    odchylenie[j] = sd(minimum)#odchylenie standardowe
+    
+    x1[powt+1] = mean(x1) #obliczanie srednich z 50 prob 
     x2[powt+1] = mean(x2)
     minimum[powt+1] = mean(minimum)
     czas[powt+1] = mean(czas)
@@ -135,18 +150,20 @@ for(k in 1:13) #najpierw k =1, robi caly kod objety tymi klamrami { } kiedy zrob
     
     odchylenie.sr[j]=odchylenie[j]
     
+    #wprowadzanie danych z wektorow do tabeli w R data table
     tabele[[j]] <- data.table(Lp=c(1:powt,"Srednie"),Wielkosc.roju=il.robali[j],Znalezione.x1=x1,Znalezione.x2=x2, Minimum=minimum,MSE.od.minimum=blad.sk,Odchylenie.standardowe=odchylenie[j], Iteracje=iter, Czas=czas,"")
-    print(tabele[[j]]) #wrzucanie danych z wektorow do tabeli w R np. jesli Iteracje=iter to znaczy ze kolumna o nazwie Iteracje bedzie teraz miala dane z wektora iter, print wyswietla ta tabele 
+    print(tabele[[j]]) 
     
-    cat("\n\n") #odstep miedzy liniami
-  }#koniec petli z j czyli wykona sie 50 powtorzen z i i potem zostanie zmieniony roj na wiekszy 
+    cat("\n\n") 
+  }
   cat("TABELA SREDNICH Z 1 FUNKCJI", "\n\n")
-  tabela.srednich <- data.table(Nr_sredniej=c(1:5),Wielkosc.roju=il.robali,Znalezione.x1=x1.sr,Znalezione.x2=x2.sr, Minimum=minimum.sr,MSE.od.minimum=blad.sk.sr,Odchylenie.standardowe=odchylenie.sr, Iteracje=iter.sr, Czas=czas.sr)# to samo co wyzej tylko z tabela ze srednimi
+  tabela.srednich <- data.table(Nr_sredniej=c(1:5),Wielkosc.roju=il.robali,Znalezione.x1=x1.sr,Znalezione.x2=x2.sr, Minimum=minimum.sr,MSE.od.minimum=blad.sk.sr,Odchylenie.standardowe=odchylenie.sr, Iteracje=iter.sr, Czas=czas.sr)
+  # tworzenie tabeli srednich wynikow
   print(tabela.srednich)
   
-  write.xlsx(tabele,paste("D:/PSO/DE_",funkcje.nazwa[[k]],".xlsx")) #wrzuca tabele ktore sa "data tablami" o nazwie tabele do excela
-  write.xlsx(tabela.srednich,paste("D:/PSO/sr_de_",funkcje.nazwa[[k]],".xlsx")) # tu wrzuca tabele srednich
+  write.xlsx(tabele,paste("D:/PSO/DE_",funkcje.nazwa[[k]],".xlsx")) 
+  #eksport do Excela
+  write.xlsx(tabela.srednich,paste("D:/PSO/sr_de_",funkcje.nazwa[[k]],".xlsx")) 
   
-  
-  rm(x1,x2,minimum,czas,iter,tabele,blad.sk,x1.sr,x2.sr,minimum.sr,czas.sr,iter.sr,blad.sk.sr,odchylenie.sr)#usuwa niepotrzebne wektory(niekonieczne)
+  rm(x1,x2,minimum,czas,iter,tabele,blad.sk,x1.sr,x2.sr,minimum.sr,czas.sr,iter.sr,blad.sk.sr,odchylenie.sr)#usuwa niepotrzebne wektory
 }
